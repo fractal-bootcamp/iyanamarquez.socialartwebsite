39d5701d5df6674f998a96b335610d3f3d88b778
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-07-03 11:58:46 -0400

    add key

--- src/app/pages/profile/page.tsx
+++ src/app/pages/profile/page.tsx
@@ -26,8 +26,7 @@ const
                 <div>
                     <div className="-mx-4 flex flex-wrap p-8 bg-gray-100">
                         {posts.map((post: any) => (
-
-                            <div className="w-full px-4 md:w-1/2 lg:w-1/4 ">
+                            <div key={post.id} className="w-full px-4 md:w-1/2 lg:w-1/4 ">
                                 <div className="mb-9 py-8 px-7 shadow-md transition-all hover:shadow-lg sm:p-9 lg:px-6 xl:px-9 border bg-white">
                                     <h2 className="text-lg font-bold text-gray-800 mb-4" key={post.id}>{post.title}</h2>
                                     {/* <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac volutpat est.
